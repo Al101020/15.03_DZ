20a4e4e56342e23a2c8f4e330f192137187808c7
Al101020 <alex.vl.m@yandex.ru>
2025-12-04 22:15:55 +0300

    commit-3d

---
+++ src/component/Stars/Star.jsx
@@ -0,0 +1,23 @@
+function Star() {
+//   const [state, setState] = useState(0);
+// const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className='star'>
+        <div>Решение</div>
+      </div>
+    </>
+  );
+};
+
+export default Star;
+
+
+
+
+{/* <>
+<div className='stars'>
+  <div>Решение</div>
+</div>
+</> */}
