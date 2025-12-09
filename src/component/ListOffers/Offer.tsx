46ca08039de0efeba2cc9adafb0c251187c27441
Al101020 <alex.vl.m@yandex.ru>
2025-12-09 15:46:27 +1000

    commit-10r

--- src/component/ListOffers/Offer.tsx
+++ src/component/ListOffers/Offer.tsx
@@ -13,36 +13,41 @@ type props = {
   item : Item
 };
 
+const titleOffer = (title: string) => {
+  if (title.length > 50) {
+    return title.slice(50) + ' ...';
+  };
+};
+// {`btnFilter ${item === selected && 'black-wheat'}`}
+const currencyAndPrice = (currency_code: string, price: string) => {
+  if (currency_code === 'USD') {return '$' + price};
+  if (currency_code === 'EUR') {return '€' + price}; 
+  return currency_code + ' ' + price;
+};
 
+const classFromQuantity = (quantity: number) => {
+  if (quantity <= 10) {return 'level-low'} 
+  else if (quantity <= 20) {return 'level-medium'} 
+  else {return 'level-high'};  
+}
 
-const Offer: React.FC<props> = ({
-  item: {url, MainImage, title, currency_code, price, quantity }
-}) => {
-  console.log(typeof title);
-
-  // const titleOffer = (title: string) => {
-  //   if (title.length > 50) {
-  //     console.log(title);
-  //     return title.slice(50) + '...';
-  //   };
-  // };
-
-//   console.log(url);
-//   console.log(MainImage.url_570xN);
+const Offer: React.FC<props> = ({item: {url, MainImage, title, currency_code, price, quantity}
+  }) => {
 
   return (
-    <div className="item">
-      <div className="item-image">
+    <div className='item'>
+      <div className='item-image'>
         <a href={url}>
           <img src={MainImage.url_570xN} alt = {title}/>
         </a>
       </div>
-        <div className="item-details">
-          <p className="item-title">{title}</p>
-          <p className="item-price">{price}</p>
-          <p className={`item-quantity ${quantity}`}>{quantity} left</p>
-        </div>
+      <div className='item-details'>
+        <p className='item-title'>{titleOffer(title)}</p>
+        <p className='item-price'>{currencyAndPrice(currency_code, price)}</p>
+        <p className={`item-quantity ${classFromQuantity(quantity)}`}>{quantity} left</p>
+      </div>
     </div>
   )
 }
+
 export default Offer;
